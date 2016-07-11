3b15e78c9336589f3777bad1eb5f77a3d8d90974
m-imaoka <m-imaoka@probizmo.co.jp>
2016-07-10 23:35:28 -0400

    add employee index

---
+++ src/js/employee_index.js
@@ -0,0 +1,31 @@
+import m from "mithril";
+
+var Employee = {
+        list: () => {
+	  return m.request({ method: "GET", url: "http://192.168.56.10:3000/employees" });
+        }
+};
+
+
+var EmployeeIndex = {
+	//controller
+	controller: function() {
+          return {
+            employees: Employee.list()
+          };
+        },
+
+	//view
+	view: function(ctrl) {
+          return m("table", [
+            m("thead", m("tr", [m("th", "名前"), m("th", "性別")])),
+            m("tbody", ctrl.employees().map((employee) => {
+              return m("tr", [m("td", employee.name), m("td", employee.sex)]);
+            }))
+          ]);
+	}
+};
+
+
+//初期化
+m.mount(document.getElementById("content"), EmployeeIndex);
